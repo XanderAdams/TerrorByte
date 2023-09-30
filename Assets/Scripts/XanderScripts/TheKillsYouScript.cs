598efe3cf325bf50f3722fd5378b11c6bd79e682
Xander Adams <xanderadams555@gmail.com>
2023-09-30 05:25:32 -0500

    The TheKillsYouScript Script

--- Assets/Scripts/XanderScripts/TheKillsYouScript.cs
+++ Assets/Scripts/XanderScripts/TheKillsYouScript.cs
@@ -4,6 +4,32 @@ using UnityEngine;
 
 public class TheKillsYouScript : MonoBehaviour
 {
+    private float scan;
+    private File target;
+    public FileSystem fileSystem;
+    public void Scan()
+    {
+        if (fileSystem.files.Count>0)
+        {
+                if (fileSystem.files[fileSystem.files.Count-1] != target)
+            {
+                target = fileSystem.files[fileSystem.files.Count-1];
+                scan = 0;
+            }
+            else
+            {
+                scan+=1;
+            }
+
+            if(scan>=target.size)
+            {
+                scan = 0;
+                fileSystem.PopFile();
+            }
+        }
+        
+
+    }
     // Start is called before the first frame update
     void Start()
     {
