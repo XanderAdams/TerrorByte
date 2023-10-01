18dbfb0978a6a69e4cc828c556a2554265ec1076
Liam York <yorkliam08@gmail.com>
2023-09-30 22:07:28 -0500

    Enemy Attack and Game Manager

---
+++ Assets/Scripts/LiamFolder/EnemyAttack.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public TheKillsYouScript virus;
+    public int power = 1;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            for (int i = 0; i < power; i++)
+            {
+                virus.Scan();
+            }
+            Debug.Log("Hit");
+        }
+
+
+        
+    }
+}
