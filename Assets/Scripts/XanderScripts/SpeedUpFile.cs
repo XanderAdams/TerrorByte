aab223a18657e110ac935f3ca11c09a90013bf0c
Xander Adams <xanderadams555@gmail.com>
2023-10-01 00:14:02 -0500

    Speed Up File

--- Assets/Scripts/XanderScripts/SpeedUpFile.cs
+++ Assets/Scripts/XanderScripts/SpeedUpFile.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New File", menuName = "UpgradeFile/Move/Move")]
-public class MoveFile : File
+[CreateAssetMenu(fileName = "New File", menuName = "UpgradeFile/Move/Speed")]
+public class SpeedUpFile : File
 {
     
     public float baseSpeed;
@@ -13,7 +13,7 @@ public class MoveFile : File
         Debug.Log("Passive");
         if(GameObject.FindWithTag("Player").GetComponent<MovementTopDown>()!=null)
         {
-            GameObject.FindWithTag("Player").GetComponent<MovementTopDown>().moveSpeed = baseSpeed;
+            GameObject.FindWithTag("Player").GetComponent<MovementTopDown>().moveSpeed *= baseSpeed;
         }
     
     }
